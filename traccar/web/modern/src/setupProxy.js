a8d31168d9fd5c53172545095aac7fa1a03238c5
7erry <twal7ers@gmail.com>
2020-10-14 12:18:16 -0400

    duplicate

--- traccar/web/modern/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(proxy('/api/socket', { target: 'ws://' + process.env.REACT_APP_URL_NAME, ws: true }));
-    app.use(proxy('/api', { target: 'http://' + process.env.REACT_APP_URL_NAME }));
-};
